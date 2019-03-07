09b1ca165ec98a3c66e6a75186ad558c4c9d6801
nazmariam <fedirko.mary@gmail.com>
2019-03-08 01:08:04 +0200

    happy feminist

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $( document ).ready(function() {
+    var setClock = 600;
+    $('.clock').each(function() {
+        var clock = $(this);
+        clock.FlipClock(setClock, {
+            clockFace: 'HourlyCounter',
+            countdown: true,
+            language: 'it',
+            autoStart:true
+        });
+    });
+    $('select').niceSelect();
+
     if (screen.width < 768) {
         $('.slick').slick({
             slidesToShow: 2,
